734ed231b0a92ea585a725f879506b869ff3ccc8
csbb2787 <max.ninow@student.uibk.ac.at>
2025-06-03 19:13:40 +0200

    cleanup

--- sealwatch/xunet/__init__.py
+++ sealwatch/xunet/__init__.py
@@ -2,7 +2,3 @@ from .train import XuNetTrainer
 from .model import XuNet
 from .b0 import B0
 from .dataset import DatasetLoad
-from .prepare_dataset import process_csv
-from .prepare_boss import prepare_boss
-
-__all__ = [ "XuNet", "DatasetLoad", "process_csv", "prepare_boss"]
